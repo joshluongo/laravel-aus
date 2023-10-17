b514233b737f2666c091485269af179656eb6445
Josh Luongo <josh@jrapps.com.au>
2023-10-17 16:53:41 +1100

    Updated strings.

--- resources/lang/en/translations.php
+++ resources/lang/en/translations.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'abn_length' => 'This must be 11 digits long.',
-    'acn_length' => 'This must be 9 digits long.',
-    'abn_invalid' => 'This is not a valid ABN.',
-    'acn_invalid' => 'This is not a valid ACN.',
+    'abn_length' => 'The :attribute must be 11 digits long.',
+    'acn_length' => 'The :attribute must be 9 digits long.',
+    'abn_invalid' => 'The :attribute is not a valid ABN.',
+    'acn_invalid' => 'The :attribute is not a valid ACN.',
 ];
